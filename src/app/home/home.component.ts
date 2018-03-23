2a9423fba7e81d454b2d6104013040a089b0a067
swathiGiligar <swathigiligar@gmail.com>
2018-03-23 19:01:20 +0530

    inline edit for status

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,6 +4,9 @@ import { PTASK } from '../ptask/ptask.interface';
 import { DatePipe } from '@angular/common';
 import { AuthService } from '../auth/auth.service';
 import { Modes } from '../side-bar/side-bar.component';
+import { SelectItem, ConfirmationService } from 'primeng/api';
+import { Options, Status } from '../utils/options';
+import { MessageService } from 'primeng/components/common/messageservice';
 
 
 @Component({
@@ -22,10 +25,16 @@ export class HomeComponent implements OnInit {
   cols: any[];
   selectedTask: PTASK;
   mode = Modes.MY_TASKS;
-  pCol = 'p-low';
+  sortStatus = 'UNS';
+  sortOthers = 'cellStyle';
+
+  statuses: SelectItem[] = new Options().getStatuses();
+  status: Status;
 
   constructor(private pTaskService: PTASKService,
-    private auth: AuthService) { }
+    private auth: AuthService,
+    private confirmationService: ConfirmationService,
+    private messageService: MessageService) { }
 
   displayAllTasks() {
     this.cols.push({field: 'owner', header: 'Owner'});
@@ -90,7 +99,7 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
     this.cols = [
       { field: 'description', header: 'Description' },
-      { field: 'status', header: 'Status' },
+      // { field: 'status', header: 'Status' },
       { field: 'priority', header: 'Priority' },
       // { field: 'owner', header: 'Owner' },
       // { field: 'projectName', header: 'Project Name' },
@@ -102,4 +111,89 @@ export class HomeComponent implements OnInit {
 
     this.displayMyTasks();
   }
+
+  sortOnStatus() {
+    this.sortOthers = 'cellStyle';
+    if (this.sortStatus === 'ASC') {
+      this.tasks = this.tasks.sort(this.sortDescending);
+      this.sortStatus = 'DESC';
+    } else {
+      this.tasks = this.tasks.sort(this.sortAscending);
+      this.sortStatus = 'ASC';
+    }
+  }
+
+  sortAscending(aTask: PTASK, anotherTask: PTASK): number {
+    if (aTask.status < anotherTask.status) {
+      return -1;
+    }
+    if (aTask.status > anotherTask.status) {
+      return 1;
+    }
+    return 0;
+  }
+
+  sortDescending(aTask: PTASK, anotherTask: PTASK): number {
+    if (aTask.status > anotherTask.status) {
+      return -1;
+    }
+    if (aTask.status < anotherTask.status) {
+      return 1;
+    }
+    return 0;
+  }
+
+  initEdit(event) {
+    // console.log('Hit Edit ' + event.data.status);
+    this.status = {'status': event.data.status};
+  }
+
+  saveChanges(event) {
+    const task: PTASK = event.data;
+    // console.log('Hit Save ' + this.status.status);
+    this.confirmEdit(task, this.status.status);
+  }
+
+  confirmEdit(task: PTASK, newStatus: string) {
+    const oldStatus = task.status;
+    this.confirmationService.confirm({
+      message: 'Do you want to Change the status of this task from <b>' +
+                task.status + '</b> to <b>' + newStatus + '</b>?',
+      header: 'Update Confirmation',
+      icon: 'fa fa-edit',
+      accept: () => {
+          task.status = newStatus;
+          this.pTaskService.updateTask(task).subscribe(
+            res => {
+              this.showSuccessMessage('Task status updated');
+            },
+           error => {
+             this.showErrorMessage('Error updating status');
+            }
+          );
+        },
+      reject: () => {
+        this.status = {status: oldStatus};
+      }
+  });
+  }
+
+  showSuccessMessage(message: string) {
+    this.messageService.add({
+      severity: 'success',
+      summary: message
+    });
+  }
+
+  showErrorMessage(message: string) {
+    this.messageService.add({
+      severity: 'error',
+      summary: message
+    });
+  }
+
+  resetStatusHead(statusead) {
+    this.sortStatus = 'UNS';
+  }
+
 }
